e3cb2aaf4b9ac467dedafe83e442f07baf4860a2
Ivan Ivanov <slayer_word_11@outlook.com>
2015-09-09 22:32:34 +0300

    add less and grunt tasks

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["less"]
+                },
+                files: {
+                    "css/repentless.css": "less/repentless.less"
+                }
+            }
+        },
+        watch: {
+            files: [
+                "less/*.less"
+            ],
+            tasks: ['less']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
